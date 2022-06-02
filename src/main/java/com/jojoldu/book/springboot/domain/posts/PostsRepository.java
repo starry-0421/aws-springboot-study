45ae56bb8b8e7fc50e779eef18db14861e36f0c6
ebkang <ebkang@gsitm.com>
2022-06-02 09:06:52 +0900

    JPA Auditing으로 생성시간/수정시간 자동화하기

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.domain.posts.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
